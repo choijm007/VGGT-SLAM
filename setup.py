6d3d9f5871bf7ec81e5b6117c8ec12eed4f8bedf
PR5GRAMM2R <pon07151@gmail.com>
2025-05-02 01:05:28 +0900

    --all

---
+++ setup.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+from setuptools import setup
+
+import torch
+from torch.utils.cpp_extension import BuildExtension, CppExtension
+import os
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+has_cuda = torch.cuda.is_available()
+
+include_dirs = [
+    os.path.join(ROOT, "mast3r_slam/backend/include"),
+    os.path.join(ROOT, "thirdparty/eigen"),
+]
+
+sources = [
+    "mast3r_slam/backend/src/gn.cpp",
+]
+extra_compile_args = {
+    "cores": ["j8"],
+    "cxx": ["-O3"],
+}
+
+if has_cuda:
+    from torch.utils.cpp_extension import CUDAExtension
+
+    cuda_version = torch.version.cuda
+    cuda_major, cuda_minor = map(int, cuda_version.split(".")[:2])
+
+    sources.append("mast3r_slam/backend/src/gn_kernels.cu")
+    sources.append("mast3r_slam/backend/src/matching_kernels.cu")
+    extra_compile_args["nvcc"] = [
+        "-O3",
+        "-gencode=arch=compute_60,code=sm_60",
+        "-gencode=arch=compute_61,code=sm_61",
+        "-gencode=arch=compute_70,code=sm_70",
+        "-gencode=arch=compute_75,code=sm_75",
+        "-gencode=arch=compute_80,code=sm_80",
+        "-gencode=arch=compute_86,code=sm_86",
+    ]
+
+    if (cuda_major, cuda_minor) >= (11, 8):
+        extra_compile_args["nvcc"].append("-gencode=arch=compute_90,code=sm_90")
+
+    if (cuda_major, cuda_minor) >= (12, 8):
+        extra_compile_args["nvcc"].append("-gencode=arch=compute_120,code=sm_120")
+
+    ext_modules = [
+        CUDAExtension(
+            "mast3r_slam_backends",
+            include_dirs=include_dirs,
+            sources=sources,
+            extra_compile_args=extra_compile_args,
+        )
+    ]
+else:
+    print("CUDA not found, cannot compile backend!")
+
+setup(
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExtension},
+)
